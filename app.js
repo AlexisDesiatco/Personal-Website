0a063430226346db9fdd8e48c954f725dbdf0631
AlexisDesiatco <bsemc.desiatcoalexisd@gmail.com>
2022-06-20 20:54:11 +0800

    Revised

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
 const slider = document.querySelector('.gallery');
 let isDown = false;
 let startX;
